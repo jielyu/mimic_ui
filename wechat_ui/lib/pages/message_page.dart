10fd66d25d271cc60eb3af0ac2494b9333429532
wuyi <jiejielyu@outlook.com>
2022-01-13 15:52:26 +0800

    feat(lib/pages/message_page.dart):load message data from json file

--- wechat_ui/lib/pages/message_page.dart
+++ wechat_ui/lib/pages/message_page.dart
@@ -1,4 +1,33 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+/// 用于存储消息相关的内容
+class MessageInfo {
+  String name; // 显示的用户名
+  String recentMsg; // 最近一条消息
+  DateTime? recentTime; // 最近消息的时间
+
+  MessageInfo(this.name, this.recentMsg) {
+    recentTime = DateTime.now();
+  }
+
+  // 从json数据创建对象
+  MessageInfo.fromJson(Map<String, dynamic> jsonObj)
+      : name = jsonObj['username'],
+        recentMsg = jsonObj['recent_msg'] {
+    recentTime = DateTime.now();
+  }
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'recentMsg': recentMsg,
+        'recentTime': recentTime.toString()
+      };
+
+  @override
+  String toString() => json.encode(this);
+}
 
 class MessagesPage extends StatefulWidget {
   const MessagesPage({Key? key}) : super(key: key);
@@ -8,6 +37,29 @@ class MessagesPage extends StatefulWidget {
 }
 
 class _MessagesPageState extends State<MessagesPage> {
+  List<MessageInfo> msgList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // 从json文件中载入数据
+    loadJson();
+  }
+
+  void loadJson() async {
+    rootBundle.loadString('assets/data/msg_models.json').then((value) {
+      List list = json.decode(value);
+      for (var v in list) {
+        msgList.add(MessageInfo.fromJson(v));
+      }
+      // 打印生成的对象列表
+      // print(msgList.length);
+      // for (var v in msgList) {
+      //   print(v);
+      // }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
