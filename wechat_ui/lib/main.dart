3db94e7b669e6ef39f41e2ab020042b38b91f122
wuyi <jiejielyu@outlook.com>
2022-01-26 16:18:27 +0800

    feat(macos/,ios/): 增加标题栏定制，不打算完全去除标题栏

--- wechat_ui/lib/main.dart
+++ wechat_ui/lib/main.dart
@@ -1,8 +1,26 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+
 void main() {
   runApp(const MyApp());
+  // 针对桌面平台的设置
+  if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+    doWhenWindowReady(() {
+      final win = appWindow;
+      const width = 400.0;
+      const ratio = 16 / 9;
+      const height = width * ratio;
+      const initialSize = Size(width, height);
+      appWindow.minSize = initialSize;
+      appWindow.size = initialSize;
+      appWindow.alignment = Alignment.center;
+      win.title = "WeChat";
+      win.show();
+    });
+  }
 }
 
 class MyApp extends StatelessWidget {
